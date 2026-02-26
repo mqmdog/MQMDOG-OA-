eea0a05324bbf4ee41e8d759c1c94e27d124d15e
mqmdog <3374382168@qq.com>
2026-02-26 18:04:26 +0800

    Clear email configuration in settings.py
    Removed sensitive email credentials from settings.

--- OA_back/settings.py
+++ OA_back/settings.py
@@ -197,9 +197,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '3374382168@qq.com'
-EMAIL_HOST_PASSWORD = 'dephqafxyfrhcjhe'
-DEFAULT_FROM_EMAIL = '3374382168@qq.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
 
 
 
